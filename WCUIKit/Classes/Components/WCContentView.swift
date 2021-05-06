24a3db8285d2db19a46864dd5bca15df2698c57a
Pedro Alvarez <pedronalvarez@hotmail.com>
2021-05-05 22:34:58 -0300

    created empty content view

---
+++ WCUIKit/Classes/Components/WCContentView.swift
@@ -0,0 +1,29 @@
+//
+//  WCContentView.swift
+//  WCUIKit
+//
+//  Created by Pedro Alvarez on 05/05/21.
+//
+
+import UIKit
+
+public class WCContentView: UIView {
+    
+    public enum ColorStyle {
+        case white
+        
+        var color: ThemeColors {
+            switch self {
+            case .white:
+                return .whiteThemeColor
+            }
+        }
+    }
+    
+    public var style: ColorStyle = .white
+    
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        backgroundColor = style.color.rawValue
+    }
+}
