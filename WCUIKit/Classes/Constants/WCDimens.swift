af9dc556524daf5c6a699166b4c5d7054b87ec3d
pnalvarez <pedronalvarez@hotmail.com>
2021-08-08 22:18:38 -0300

    created dimensions for margins

---
+++ WCUIKit/Classes/Constants/WCDimens.swift
@@ -0,0 +1,31 @@
+//
+//  WCDimens.swift
+//  WCUIKit
+//
+//  Created by Pedro Alvarez on 08/08/21.
+//
+
+public enum WCDimens {
+    
+    public enum Margins {
+        static let smallest: CGFloat = 4.0
+        static let verySmall: CGFloat = 8.0
+        static let small: CGFloat = 12.0
+        static let medium: CGFloat = 16.0
+        static let normal: CGFloat = 20.0
+        static let `default`: CGFloat = 24.0
+        static let bitLarger: CGFloat = 32.0
+        static let larger: CGFloat = 40.0
+        static let large: CGFloat = 48.0
+        static let veryLarge: CGFloat = 60.0
+        static let big: CGFloat = 72.0
+        static let bigger: CGFloat = 84.0
+        static let biggest: CGFloat = 96.0
+    }
+    
+    public enum Sizing {
+        static let smallest: CGFloat = 4.0
+        static let small: CGFloat = 16.0
+        static let big: CGFloat = 96.0
+    }
+}
